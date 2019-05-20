b11dfcff6fc2d6cd35716843ce2c16b93e14f834
Zhmen6ka <50750900+Zhmen6ka@users.noreply.github.com>
2019-05-20 23:07:16 +0300

    Оптимизирована программа Distance

--- Sandbox/src/main/java/ru/stqa/pft/tasks/second/Distance.java
+++ Sandbox/src/main/java/ru/stqa/pft/tasks/second/Distance.java
@@ -4,20 +4,11 @@ public class Distance {
 
   public static void main(String[] args) {
 
-    Point p1 = new Point();
-    Point p2 = new Point();
-    p1.x = -2.4;
-    p1.y = 1;
-    p2.x = 7;
-    p2.y = 5.59;
+    Point p1 = new Point(-2.4, 1);
+    Point p2 = new Point(7, 5.8);
 
     System.out.println("Расстояние между двумя точками " + p1.x + "," + p1.y + " и " + p2.x + "," + p2.y +
-            " на плоскости = " + distance(p1, p2));
-  }
-
-  public static double distance(Point p1, Point p2){
-
-    return Math.sqrt((p1.x - p2.x) * (p1.y - p2.y) + (p1.x - p2.x) * (p1.y - p2.y));
+            " на плоскости = " + p1.distance(p2));
   }
 
 }
--- Sandbox/src/main/java/ru/stqa/pft/tasks/second/Point.java
+++ Sandbox/src/main/java/ru/stqa/pft/tasks/second/Point.java
@@ -4,4 +4,13 @@ public class Point {
   public double x;
   public double y;
 
+  public Point(double x, double y){
+    this.x = x;
+    this.y = y;
+  }
+
+  public double distance(Point p2){
+
+    return Math.sqrt((this.x - p2.x) * (this.y - p2.y) + (this.x - p2.x) * (this.y - p2.y));
+  }
 }
