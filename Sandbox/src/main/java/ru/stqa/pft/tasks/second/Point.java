69de8e02bd30558b17f85dcfb67cefc3ca787e3f
Zhmen6ka <50750900+Zhmen6ka@users.noreply.github.com>
2019-05-22 18:36:45 +0300

    Исправлена формула

--- Sandbox/src/main/java/ru/stqa/pft/tasks/second/Point.java
+++ Sandbox/src/main/java/ru/stqa/pft/tasks/second/Point.java
@@ -11,6 +11,6 @@ public class Point {
 
   public double distance(Point p2){
 
-    return Math.sqrt((this.x - p2.x) * (this.y - p2.y) + (this.x - p2.x) * (this.y - p2.y));
+    return Math.sqrt((this.x - p2.x) * (this.x - p2.x) + (this.y - p2.y) * (this.y - p2.y));
   }
 }
