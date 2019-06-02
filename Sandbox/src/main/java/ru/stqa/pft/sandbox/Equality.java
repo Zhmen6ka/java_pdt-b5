52e302ce378e332f9c3aa484bd3476520fa69118
Zhmen6ka <50750900+Zhmen6ka@users.noreply.github.com>
2019-06-02 17:22:20 +0300

    В песочнице создан класс Equality

--- Sandbox/src/main/java/ru/stqa/pft/sandbox/Equality.java
+++ Sandbox/src/main/java/ru/stqa/pft/sandbox/Equality.java
@@ -2,8 +2,8 @@ package ru.stqa.pft.sandbox;
 
 public class Equality {
   public static void main(String[] args){
-    String s1 = "firefox";
-    String s2 = s1;
+    String s1 = "firefox 2.0";
+    String s2 = "firefox " + Math.sqrt(4.0);
 
     System.out.println(s1 == s2);
     System.out.println(s1.equals(s2));
