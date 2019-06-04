d8d24fb0eab7c8cc6f869196281d6c1b7c7a3528
Zhmen6ka <50750900+Zhmen6ka@users.noreply.github.com>
2019-06-04 19:54:15 +0300

    Выполнена оптимизация заполнения форм: неизменяемые значения в полях остаются

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -10,7 +10,7 @@ public class GroupModificationTests extends TestBase {
     app.getNavigationHelper().gotoGroupPage();
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().initGroupModification();
-    app.getGroupHelper().fillGroupForm(new GroupData("TestGroup", "testModification", "t Mod"));
+    app.getGroupHelper().fillGroupForm(new GroupData("Test1", "test2", "test3"));
     app.getGroupHelper().submitGroupModification();
     app.getGroupHelper().returnToGroupPage();
 
